4c8f679f807d86475d66a2a63c74a505cb10ec60
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2018-11-16 03:08:56 -0200

    Implemented event bindings, resolvers and pagination.

--- src/app/photos/photo-list/photos-list.component.ts
+++ src/app/photos/photo-list/photos-list.component.ts
@@ -1,26 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photos-list',
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.css']
 })
-export class PhotosListComponent implements OnInit {
-
-  photos: Photo[] = [];
+export class PhotosListComponent implements OnInit, OnDestroy {
   
+  
+  photos: Photo[] = [];
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: Boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
+
   constructor(
-    private photoService : PhotoService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
     ){ }
   
-  ngOnInit(): void{
+  ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data['photos'];
     
-    const userName = this.activatedRoute.snapshot.params.userName
-    this.photoService.listFromUser( userName )
-      .subscribe(photos => this.photos = photos);    
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage )
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) this.hasMore = false;
+      });
+    
   }
 }
