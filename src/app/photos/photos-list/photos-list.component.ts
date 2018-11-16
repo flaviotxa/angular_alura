86d7dac574463fba73cfef57761655612a70e727
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2018-11-15 23:46:04 -0200

    Standardization of the code

---
+++ src/app/photos/photos-list/photos-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo/photo.service';
+
+@Component({
+  selector: 'app-photos-list',
+  templateUrl: './photos-list.component.html',
+  styleUrls: ['./photos-list.component.css']
+})
+export class PhotosListComponent implements OnInit {
+
+  photos: any[] = [];
+  
+  constructor(private photoService : PhotoService){}
+  
+  ngOnInit(): void{
+    this.photoService.listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);    
+  }
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotosListComponent } from './photos-list/photos-list.component';
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
+    declarations: [ 
+        PhotoComponent, 
+        PhotosListComponent 
+    ],
     imports: [ HttpClientModule ]
 })
 export class PhotosModule {}    
