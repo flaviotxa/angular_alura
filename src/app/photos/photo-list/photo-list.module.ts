791bd538837eef97569153870bfccf0f75b4f3ca
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2018-11-20 14:01:09 -0200

    Converted search to a component and applied output property to keep it working properly

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,13 +7,15 @@ import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { SearchCompoent } from './search/search.component';
 
 @NgModule({
     declarations: [
         PhotosListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescriptionPipe
+        FilterByDescriptionPipe,
+        SearchCompoent
     ],
     imports: [
         CommonModule,
--- src/app/photos/photo-list/photos-list.component.ts
+++ src/app/photos/photo-list/photos-list.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -11,12 +9,11 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.css']
 })
-export class PhotosListComponent implements OnInit, OnDestroy {
+export class PhotosListComponent implements OnInit {
   
   
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: Boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -29,20 +26,13 @@ export class PhotosListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    
-    this.debounce
-    .pipe(debounceTime(300))
-    .subscribe(filter => this.filter = filter);
-  }
-
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
   }
 
   load() {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage )
       .subscribe(photos => {
+        this.filter = ''; 
         this.photos = this.photos.concat(photos);
         if(!photos.length) this.hasMore = false;
       });
