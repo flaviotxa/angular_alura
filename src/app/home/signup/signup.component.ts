355e80855c1e99b805c6ca3c58f529a46f1aa59a
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2018-12-15 15:54:09 -0200

    Created validators for the form, async validation and data submition

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,6 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { SignUpService } from './signup.service';
+import { Router } from '@angular/router';
+
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
 
 @Component({
     templateUrl: './signup.component.html'
 })
-export class SignUpComponent { }
+export class SignUpComponent implements OnInit { 
+    
+    signupForm: FormGroup;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private userNotTakenValidadorService: UserNotTakenValidatorService,
+        private signUpService: SignUpService,
+        private router: Router) { }
+
+    ngOnInit(): void {
+        this.signupForm = this.formBuilder.group({
+            email: ['',
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            fullName: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(40)
+                ]
+            ],
+            userName: ['', 
+                [
+                    Validators.required,
+                    lowerCaseValidator,
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ],
+                this.userNotTakenValidadorService.checkUserNameTaken()
+            ],
+            password: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(14)
+                ]
+            ]
+      
+        })  
+    }
+
+    signup(){
+        const newUser = this.signupForm.getRawValue() as NewUser;
+        this.signUpService
+            .signup(newUser)
+            .subscribe(
+                ()=> this.router.navigate(['']),
+                err => console.log(err)
+            );
+    }
+
+}
